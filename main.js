035957c95be10462b0eb44767e8775da8cb47b91
mafatah <m_afatah@hotmail.com>
2025-05-22 08:06:40 +0300

    Add minimal Electron main file

---
+++ main.js
@@ -0,0 +1,15 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+
+  win.loadFile('index.html') // make sure this file exists or change this
+}
+
+app.whenReady().then(createWindow)
